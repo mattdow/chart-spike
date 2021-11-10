9039053fc62b53a2768f08983da7f520a53c352b
DoctorHowser <doctorhowser@gmail.com>
2021-11-10 09:28:35 -0600

    add comment

--- server/modules/authentication-middleware.js
+++ server/modules/authentication-middleware.js
@@ -11,7 +11,9 @@ const rejectUnauthenticated = (req, res, next) => {
 };
 
 const rejectNonAdmin = (req, res, next) => {
-  // check if logged in
+  // check if user has proper access
+  // this requires that they are logged in but does not check!
+  // will error if you dont also use rejectUnauthenticated
   if (req.user.access_level >= 0) {
     // They were authenticated! User may do the next thing
     // Note! They may not be Authorized to do all things
